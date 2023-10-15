1b78859c3e1f489bc685cefc2aa7509bacc28259
David Dickinson <davidrd123@gmail.com>
2023-10-15 11:44:47 -0700

    Updated to use my y-sweet cloud connection string

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import { CodeEditor } from './CodeEditor'
 import { YDocProvider } from '@y-sweet/react'
 import { getOrCreateDoc } from '@y-sweet/sdk'
 
-import {CONNECTION_STRING} from '@/lib/config'
+// import {CONNECTION_STRING} from '@/lib/config'
 
 type HomeProps = {
   searchParams: Record<string, string>
 }
 
-// const CONNECTION_STRING = "yss://j50rr8Tdqb5gfNfg_rw.AAAg_-VBX9RjPa4QCnMpHUXlyhPcTMJM1DSWuo86qNyaaxY@prod.y-sweet.net/p/gyaINP7dTZ4IaY3BGiI/"
+const CONNECTION_STRING = "yss://j50rr8Tdqb5gfNfg_rw.AAAg_-VBX9RjPa4QCnMpHUXlyhPcTMJM1DSWuo86qNyaaxY@prod.y-sweet.net/p/gyaINP7dTZ4IaY3BGiI/"
 // const CONNECTION_STRING = "ys://127.0.0.1:8080"
 
 export default async function Home({ searchParams }: HomeProps) {
